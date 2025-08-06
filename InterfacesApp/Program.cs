6d703557bddf3937344874d252acb3fe69320635
ABBEY EMMANUEL NII ABBEY <enaabbey003@st.ug.edu.gh>
2025-08-06 18:13:09 +0000

    Task 3:Interface IMovable with Car & Bicycle

--- InterfacesApp/Program.cs
+++ InterfacesApp/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace InterfacesApp
+{
+    // 1. Interface
+    interface IMovable
+    {
+        void Move();
+    }
+
+    // 2. Implementers
+    class Car : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("Car is moving");
+        }
+    }
+
+    class Bicycle : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("Bicycle is moving");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IMovable car = new Car();
+            IMovable bike = new Bicycle();
+
+            car.Move();   // prints "Car is moving"
+            bike.Move();  // prints "Bicycle is moving"
+        }
+    }
+}
