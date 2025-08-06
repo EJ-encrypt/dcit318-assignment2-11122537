5267036f339484f375896a2bf2a296a87d33bf1e
ABBEY EMMANUEL NII ABBEY <enaabbey003@st.ug.edu.gh>
2025-08-06 18:01:13 +0000

    Task 1:Inheritance and method overriding example

--- InheritanceApp/Program.cs
+++ InheritanceApp/Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace InheritanceApp
+{
+    // 1. Base class
+    class Animal
+    {
+        public virtual void MakeSound()
+        {
+            Console.WriteLine("Some generic sound");
+        }
+    }
+
+    // 2. Derived classes
+    class Dog : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Bark");
+        }
+    }
+
+    class Cat : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Meow");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal generic = new Animal();
+            Dog dog = new Dog();
+            Cat cat = new Cat();
+
+            generic.MakeSound();  // prints "Some generic sound"
+            dog.MakeSound();      // prints "Bark"
+            cat.MakeSound();      // prints "Meow"
+        }
+    }
+}
